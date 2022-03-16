45c129a2cf7b265e84003e2cc89847e7fab049e8
MuriloM <muriloommaia@gmail.com>
2022-03-16 17:31:14 -0300

    feat: create utils
    status-code and message-status

---
+++ app/backend/src/utils/utils.ts
@@ -0,0 +1,14 @@
+enum StatusCode {
+  badRequest = 400,
+  ok = 200,
+  internalServerError = 500,
+}
+
+enum MessagesStatus {
+  internalServerError = 'Internal server error',
+}
+
+export {
+  StatusCode,
+  MessagesStatus,
+};
