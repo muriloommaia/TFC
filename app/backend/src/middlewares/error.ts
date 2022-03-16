ac146bcfcd33438981de015a201287adcafbcae8
MuriloM <muriloommaia@gmail.com>
2022-03-16 17:30:37 -0300

    feat: create error middleware

---
+++ app/backend/src/middlewares/error.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import { MessagesStatus, StatusCode } from '../utils/utils';
+
+const errorMiddleware = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { name, message } = err;
+  switch (name) {
+    case 'ValidationError':
+      res.status(StatusCode.badRequest).json({ message });
+      break;
+
+    default:
+      res.status(StatusCode.internalServerError).json({
+        message: MessagesStatus.internalServerError,
+      });
+      break;
+  }
+  next();
+};
+
+export default errorMiddleware;
