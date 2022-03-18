14b311c53519db55b0aa808e6a8e155dd1c0a28a
MuriloM <muriloommaia@gmail.com>
2022-03-18 15:02:54 -0300

    feat: test coverage
    coverage: 96.32
    lines: 96.17

---
+++ app/backend/src/tests/clubs.test.ts
@@ -0,0 +1,68 @@
+import * as chai from 'chai';
+import { Model } from 'sequelize/types';
+import { Response } from 'superagent';
+import { app } from '../app';
+import Clubs from '../database/models/Clubs';
+import { ClubsType } from '../domain';
+import { MessagesStatus, StatusCode } from '../utils/utils';
+import { clubsFindAllMock } from './mocks/clubsMocks';
+import { correctLogin, invalidToken } from './mocks/loginMocks';
+import chaiHttp = require('chai-http');
+import Sinon = require('sinon');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testes da rota /clubs', () => {
+  let chaiHttpResponse: Response;
+  describe('findAllVerification', () => {
+    let token: string;
+    before(async () => {
+      Sinon
+        .stub(Clubs, "findAll")
+        .resolves(clubsFindAllMock as unknown as Model<ClubsType>[]);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send(correctLogin);
+
+      token = chaiHttpResponse.body.token;
+    });
+
+    after(() => {
+      (Clubs.findAll as sinon.SinonStub).restore();
+    })
+
+    it('Verify when the token is invalid', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/clubs')
+        .set('Authorization', invalidToken);
+      expect(chaiHttpResponse.status).to.be.equal(StatusCode.badRequest);
+      expect(chaiHttpResponse.body).to.be.a.property('message');
+      expect(chaiHttpResponse.body.message).to.be.equal(MessagesStatus.invalidToken);
+    });
+    it('Verify when the token doesn\'t exist', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/clubs')
+      expect(chaiHttpResponse.status).to.be.equal(StatusCode.badRequest);
+      expect(chaiHttpResponse.body).to.be.a.property('message');
+      expect(chaiHttpResponse.body.message).to.be.equal(MessagesStatus.tokenNotFound);
+    });
+
+    it('Verify if return is correct', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/clubs')
+        .set('Authorization', token);
+
+      expect(chaiHttpResponse.status).to.be.equal(StatusCode.ok);
+      expect(chaiHttpResponse.body).to.be.a('array');
+      expect(chaiHttpResponse.body[0].clubName).to.be.equal(clubsFindAllMock[0].clubName);
+      expect(chaiHttpResponse.body).deep.equal(clubsFindAllMock);
+    });
+  })
+});
---
+++ app/backend/src/tests/mocks/clubsMocks.ts
@@ -0,0 +1,14 @@
+export const clubsFindAllMock = [
+  {
+    "id": 1,
+    "clubName": "Avaí/Kindermann"
+  },
+  {
+    "id": 2,
+    "clubName": "Bahia"
+  },
+  {
+    "id": 3,
+    "clubName": "Botafogo"
+  }
+]
